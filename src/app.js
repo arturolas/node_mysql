50b5d58fbfb36e95e414746d4855d80750fdb7ac
arturolas <arturolas@hotmail.com>
2024-08-24 15:23:54 +0200

    Servidor operativo

---
+++ src/app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const cors = require('express');
+
+dotenv.config();
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Backend con NodeJs - Express + CRUD API REST + MySQL');
+});
+
+app.listen(port, () => {
+    console.log("Port ==> ", port)
+})
+
