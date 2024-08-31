62c340a5d063779efed62bd187640921140b03bd
arturolas <arturolas@hotmail.com>
2024-08-31 14:02:01 +0200

    Sequelize-ORM

--- src/app.js
+++ src/app.js
@@ -7,12 +7,16 @@ const app = express();
 
 const port = process.env.PORT || 3000;
 
+const routerApi = require('./routes');
+
 app.use(cors());
 app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send('Backend con NodeJs - Express + CRUD API REST + MySQL');
 });
+
+routerApi(app);
 
 app.listen(port, () => {
     console.log("Port ==> ", port);
---
+++ src/db/models/index.js
@@ -0,0 +1,8 @@
+const { Person, PersonSchema } = require('./persons.model');
+
+function setupModels(sequelize) {
+    
+    Person.init(PersonSchema, Person.config(sequelize));
+}
+
+module.exports = setupModels;
